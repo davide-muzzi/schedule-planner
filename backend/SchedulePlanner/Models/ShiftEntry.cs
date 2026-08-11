6e9b5d0ba95a7bcf63850e7c467e58f7ae13d4c0
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-08-11 10:21:50 +0200

    Define Models

---
+++ backend/SchedulePlanner/Models/ShiftEntry.cs
@@ -0,0 +1,9 @@
+namespace SchedulePlanner.Models;
+
+public class ShiftEntry
+{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+}
