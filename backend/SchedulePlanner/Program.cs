2ff7fd3c06ed13839e2e6e6b3a6dd6d27e7352e1
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-08-11 16:10:52 +0200

    Remove duplicate builder

--- backend/SchedulePlanner/Program.cs
+++ backend/SchedulePlanner/Program.cs
@@ -4,7 +4,12 @@ using SchedulePlanner.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ScheduleContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
