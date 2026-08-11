1be9295f6d4c1338973334c78790b2e8b5ed8ab9
Davide Muzzí <davidemax.muzzi@gmail.com>
2026-08-11 16:32:58 +0200

    Create api.js file

---
+++ frontend/src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api/scheduleentries`,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getAll() {
+    return apiClient.get('/')
+  },
+  getById(id) {
+    return apiClient.get(`/${id}`)
+  },
+  create(entry) {
+    return apiClient.post('/', entry)
+  },
+  update(id, entry) {
+    return apiClient.put(`/${id}`, entry)
+  },
+  delete(id) {
+    return apiClient.delete(`/${id}`)
+  }
+}
